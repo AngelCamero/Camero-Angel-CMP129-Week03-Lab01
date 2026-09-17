0a7defb392cf3674da3adc0ba5bfcb7cf147413e
AngelCamero <camero.angel@student.ccm.edu>
2026-09-17 16:37:45 -0400

    Add fields and methods to Date class

--- Date.java
+++ Date.java
@@ -1,3 +1,31 @@
 public class Date {
-    
+    private int day;
+    private int month;
+    private int year;
+
+    public int getDay(){
+        return this.day;
+    }
+    public int getMonth(){
+        return this.month;
+    }
+    public int getYear(){
+        return this.year;
+    }
+
+    public void setMonth(int month){
+        this.month = month;
+    }
+
+    public void setDay(int day){
+        this.day = day;
+    }
+
+    public void setYear(int year){
+        this.year = year;
+    }
+
+    public String DisplayDate(){
+        return "date entered: " + this.month + "/" + this.day + "/" + this.year;
+    }
 }

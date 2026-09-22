d6f69e0cbd1461eeebb273c1868f1d57413973fd
AngelCamero <camero.angel@student.ccm.edu>
2026-09-22 17:03:45 -0400

    Implement Employee class with attributes and methods
    Added fields and methods for employee details and constructors.

--- Employee.java
+++ Employee.java
@@ -1,3 +1,54 @@
 public class Employee {
-    
+    private String name;
+    private int idNumber;
+    private String department;
+    private String position;
+
+    public Employee(String Name, int idNumber,String department, String position){
+        setName(Name);
+        setIdNumber(idNumber);
+        setDepartment(department);
+        setPosition(position);
+    }
+    public String getName(){
+        return this.name;
+    }
+    public int getIdNumber(){
+        return this.idNumber;
+    }
+    public String getDepartment(){
+        return this.department;
+    }
+    public String getPosition(){
+        return this.position;
+    }
+    public void setName(String name){
+        this.name = name;
+    }
+    public void setIdNumber(int IDNumber){
+        this.idNumber = IDNumber;
+    }
+    public void setDepartment(String Department){
+        this.department = Department;
+    }
+    public void setPosition(String position){
+        this.position = position;
+    }
+    public Employee(String name, int idNumber){
+        this.name = name;
+        this.idNumber = idNumber;
+        this.department = "";
+        this.position = "";
+    }
+
+    public Employee(){
+        this.name = "";
+        this.idNumber = 0;
+        this.department = "";
+        this.position = "";
+    }
+
+    public void displayInfo(){
+        System.out.println("Name: " + this.name + "     IDNumber: " + this.idNumber + "     Department: " + this.department + "     Position: " + this.position);
+    }
 }

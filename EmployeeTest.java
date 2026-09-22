15e79310d51501c8da1cb8dc19e0708978995fd2
AngelCamero <camero.angel@student.ccm.edu>
2026-09-22 17:28:36 -0400

    Finished with everything
    Added user input functionality and employee information display.

--- EmployeeTest.java
+++ EmployeeTest.java
@@ -1,3 +1,29 @@
+import java.util.Scanner;
 public class EmployeeTest {
-    
+    String name;
+    int idNumber;
+    String department;
+    String position;
+
+    public static void main(String [] args){
+        Scanner scanner = new Scanner(System.in);
+        System.out.print("Please enter your full name: ");
+        String name = scanner.nextLine();
+
+        System.out.print("Please enter your ID number: ");
+        int idNumber = scanner.nextInt();
+        scanner.nextLine();
+
+        System.out.print("Please enter the name of the department you work in: ");
+        String department = scanner.nextLine();
+
+        System.out.print("Please enter the name of your position: ");
+        String position = scanner.nextLine();
+
+
+        Employee employee = new Employee(name, idNumber, department, position);
+        System.out.println();
+
+        employee.displayInfo();
+    }
 }
